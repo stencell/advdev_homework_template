ff51007d66e170139e1ff97e8e3e256f2573739d
stencell <nate@redhat.com>
2019-09-26 19:34:45 -0300

    .

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,10 +15,14 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 # TBD
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+echo "Pause rollout"
 oc rollout pause dc jenkins
+echo "Set resources"
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
+echo "set probes"
 oc set probe dc jenkins --liveness get-url=http://:8080/login --initial-delay-seconds=120 --timeout-seconds=30 --period-seconds=30 --success-threshold=1 --failure-threshold=2
 oc set probe dc jenkins --readiness get-url=http://:8080/login --initial-delay-seconds=3 --timeout-seconds=30 --period-seconds=10 --success-threshold=1 --failure-threshold=3
+echo "resume rollout"
 oc rollout resume dc jenkins
 
 # Create custom agent container image with skopeo
